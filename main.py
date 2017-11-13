8f74aa13e1c1d24e41a618a9f65c68527d70f541
tmazza <tiagomdepaula@gmail.com>
2017-11-13 16:14:08 -0200

    Regressão logistica

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from random import random
-from math import exp
+from math import exp, log
 
 feature = [
 	lambda i: 1,
@@ -9,10 +9,11 @@ feature = [
 	lambda i: sin(i[0]), #x1
 ]
 
-# C: lista de coeficientes da hipotese (thetas)
+# C: lista de coeficientes da hipótese (thetas)
 # I: uma instância dos dados de entrada
 def hipotese(C, X):
-  return sum( [ c*feature[i](X) for i, c in enumerate(C)] )
+  f = sum( [ c*feature[i](X) for i, c in enumerate(C)] )
+  return 1 / ( 1 + exp(-f))
 
 # Retorna valores normalizados
 def getData():
@@ -44,15 +45,16 @@ def getData():
 
 data = getData()
 
-print(data)
-
 # Média dos erros ao quadrado
-# C: lista de coeficientes da hipotese (thetas)
+# C: lista de coeficientes da hipótese (thetas)
 def J(C):
-  return (1/(2*len(data))) * sum([ (hipotese(C, X) - X[len(X)-1])**2 for X in data ])
+  def fn(X):
+    y = X[len(X)-1]
+    return -y*(log( hipotese(C, X) )) - (1-y)*(log( 1-hipotese(C, X) ))
+  return (1/(2*len(data))) * sum([ fn(X) for X in data ])
 
 # Cálculo da derivada em relação a um dos coeficientes (aproximação numérica)
-# C: lista de coeficientes da hipotese (thetas)
+# C: lista de coeficientes da hipótese (thetas)
 # i: índice do coefiente que derivará J
 def derivada_raw(C, i):
   epsilon = 0.0000001
@@ -62,9 +64,8 @@ def derivada_raw(C, i):
   C2[i] = C2[i] - epsilon
   return (J(C1) - J(C2)) / (2*epsilon)
 
-
 # Cálculo da derivada em relação a um dos coeficientes (usando função de erro)
-# C: lista de coeficientes da hipotese (thetas)
+# C: lista de coeficientes da hipótese (thetas)
 # i: índice do coefiente que derivará J
 def derivada(C, i):
   return (1/len(data)) * sum([ (hipotese(C, X) - X[len(X)-1])*feature[i](X) for X in data ]) # onde X[len[X]-1] é o atributo alvo ou y
@@ -77,11 +78,24 @@ coeficiente = [
 alpha = 0.1;
 
 erro_atual = 0;
-while(abs(J(coeficiente) - erro_atual) > 0.001):
+while(abs(J(coeficiente) - erro_atual) > 0.00001):
   
   erro_atual = J(coeficiente);
 
   print("Erro :", J(coeficiente), "\t\tC: ", coeficiente)
   
   # Atualização de coeficientes
-  coeficiente = [ c-alpha*derivada(coeficiente, i) for i,c in enumerate(coeficiente) ]
+  coeficiente = [ c - alpha*derivada(coeficiente, i) for i,c in enumerate(coeficiente) ]
+
+
+print(hipotese(coeficiente, [0.1]))
+print(hipotese(coeficiente, [0.2]))
+print(hipotese(coeficiente, [0.3]))
+print(hipotese(coeficiente, [0.4]))
+print(hipotese(coeficiente, [0.5]))
+print(hipotese(coeficiente, [0.5]))
+print(hipotese(coeficiente, [0.6]))
+print(hipotese(coeficiente, [0.7]))
+print(hipotese(coeficiente, [0.8]))
+print(hipotese(coeficiente, [0.9]))
+print(hipotese(coeficiente, [0.10]))
