f2bf4e87b7471ca8401cf4dc19ee8c15ea3927b1
tmazza <tiagomdepaula@gmail.com>
2017-11-17 14:07:24 -0200

    Rede neural (camadas e neurônios estáticos)

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ from random import random
 from math import exp, log
 
 feature = [
-	lambda i: i[0], # x1
-  lambda i: i[1], # x2
-  #lambda i: i[0]**2, # x1 ^ 2 
-  #lambda i: i[1]**5, # x2 ^ 5
-	#lambda i: sin(i[0]), # sin(x1)
+	lambda x: x[0], # x1
+  lambda x: x[1], # x2
+  lambda x: x[2], # x3
 ]
 
 # C: lista de coeficientes da hipótese (thetas)
@@ -19,7 +17,7 @@ def hipotese(C, X):
 def getData():
 
   def fn_para_teste(x1, x2):
-    return 3*x1 - 2*x2;
+    return x1 + x2;
 
   data = [ 
     [1.17, 2.17,  fn_para_teste(1.17, 2.17)],
@@ -74,36 +72,87 @@ def derivada_raw(C, i):
 def derivada(C, i):
   return (1/len(data)) * sum([ (hipotese(C, X) - X[len(X)-1])*feature[i](X) for X in data ]) # onde X[len[X]-1] é o atributo alvo ou y
 
+# coeficiente = [
+#   [ [ random(), random() ], [ random(), random()] ], # camada 1
+#   [ [ random(), random() ] ],                        # camada 2
+# ]
 coeficiente = [
-  [ [ random(), random() ], [ random(), random()] ], # camada 1
-  [ [ random(), random() ] ],                        # camada 2
+  [ [ 1, 2 ], [ 3, 4 ], [ 1, 2 ] ], # camada 1
+  [ [ 1, 2, 1 ], [ 3, 4, 1 ], [ 1, 2, 1 ] ], # camada 1
+  [ [ 5, 6, 1 ] ], # camada 2
 ]
+
+###
 
 neuronio = [ # valores iniciais quaisquer, serão atribuidos na propagação das entradas
-  [ 0, 0 ], # camada 1
-  [ 0 ],    # camada 2
+  [ 0, 0, 0 ], # camada 1
+  [ 0, 0, 0 ], # camada 1
+  [ 0 ], # camada 2
 ]
 
-X = data[1] # (teste) usando somente um instância dos dados
+erro = [ # valores iniciais quaisquer, serão atribuidos na propagação das entradas
+  [ 0, 0, 0 ], # camada 1 (oculta)
+  [ 0, 0, 0 ], # camada 1 (oculta)
+  [ 0 ],    # camada 2 (saída)
+]
 
-y = X[ len(X)-1 ]
-print("X", X)
+ultima_camada = len(neuronio)-1;
+print("ultima_camada", ultima_camada)
 
-C = coeficiente[0][0];
-neuronio[0][0] = hipotese(C, X)
-print("camada 0 - neuronio 0", C)
+for i,X in enumerate(data):
+  print(i,'-',X)
+  # X = data[1] # (teste) usando somente um instância dos dados
 
-C = coeficiente[0][1]
-neuronio[0][1] = hipotese(C, X)
-print("camada 0 - neuronio 1", C)
+  # ---- Propagação de entradas
 
-C = coeficiente[1][0];
-neuronio[1][0] = hipotese(C, neuronio[0]);
+  y = X[ len(X)-1 ]
+
+  for l in range(0, len(neuronio)): # camadas
+    for i in range(0, len(neuronio[l])): # neurônios da camada
+      if(l == 0):
+        neuronio[l][i] = hipotese(coeficiente[l][i], X)             # 1º camada tem como entrada X
+      else:
+        neuronio[l][i] = hipotese(coeficiente[l][i], neuronio[l-1]) # demais camada têm como entrada a camada anterior
+
+  print("Valores propagados:", neuronio)
 
 
-f_x = neuronio[ len(neuronio)-1 ][0] # saída último neurônio
-print("f_x", f_x)
-print("erro", abs(f_x - y))
+  # ---- Cálculo erros
 
-print(neuronio)
-print(neuronio[ len(neuronio)-1 ][0])
+  # camada de saída (considera somente 1 saída (TODO: permitir mais de uma saida))
+  erro[ultima_camada][0] = neuronio[ultima_camada][0] - y 
+
+  # camadas ocultas
+  for l in range(len(neuronio)-2, -1, -1): # camadas (da penultima para a primeira, última camada já foi tratada no passo anterior).
+    for i in range(0, len(neuronio[l])): # neurônios da camada
+      coluna_pesos = [row[i] for row in coeficiente[l+1]] # usa somente pesos do neurônio i (coeficiente[l+1] retorna os pesos de todos os coeficientes da camada da frente, pesos de um neuronio estão na coluna dessa matriz)
+      erros_balanceados = sum([ c*erro[l+1][j] for j,c in enumerate(coluna_pesos) ])
+      erro[l][i] = erros_balanceados * (neuronio[l][i] * (1 - neuronio[l][i]))  # somatorio dos erros dos neuronios das camadas seguintes
+
+  print("Erros coletados:", erro)
+
+
+  # ---- Cálculo gradientes / Ajuste coeficientes(pesos)
+
+  print('---------------------')
+
+  # TODO: reavaliar!!!
+  # TODO: tentar unificar X e neuronio (trata X como camda zero e demais como camada 1) 
+
+  alpha = 0.1
+
+  print('l i j \told\tnew')
+  for l in range(0, len(neuronio)):
+    for i in range(0, len(neuronio[l])):
+      for j in range(0, len(coeficiente[l][i])):
+        if(l == 0):
+          new = coeficiente[l][i][j] - alpha * ( X[j] * erro[l][i] )
+          print(l, i, j, '\t', coeficiente[l][i][j], '\t', new, '\t', X[j], '\t', erro[l][i])
+          coeficiente[l][i][j] = new
+        else:
+          new = coeficiente[l][i][j] - alpha * ( neuronio[l-1][j] * erro[l][i] )
+          coeficiente[l][i][j] = new
+          print(l, i, j, '\t', coeficiente[l][i][j], '\t', new, '\t', neuronio[l-1][j], '\t', erro[l][i])
+
+  print('---------------------')
+
